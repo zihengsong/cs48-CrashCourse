a0da19ef164e92c36499582b55611a8c8438f2aa
madhukannan <mkannan@umail.ucsb.edu>
2017-02-19 22:37:25 -0800

    added conflict handling for scheduler class

--- src/ucsbCurriculum/Utility/Util.java
+++ src/ucsbCurriculum/Utility/Util.java
@@ -12,18 +12,28 @@ public class Util {
 	}
 	
 //  save for later, this can be used for compare when adding classes
-//	static boolean compare(ArrayList<Time> time1, ArrayList<Time> time2){
-//		int len1 = time1.size();
-//		int len2 = time2.size();
-//		
-//		for(int i = 0; i < len1; i++)
-//			for(int j = 0; j < len2; j++)
-//				if(have_conflict(time1.get(i), time2.get(j)))
-//					// TO-DO: delete that element in the second list
-//					
+	public static boolean compare(ArrayList<Time> time1, ArrayList<Time> time2){
+		int len1 = time1.size();
+		int len2 = time2.size();
+		
+		for(int i = 0; i < len1; i++){
+			for(int j = 0; j < len2; j++){
+				if(have_conflict(time1.get(i), time2.get(j))){
+					// TO-DO: delete that element in the second list
+					time2.remove(j);
+				}
+			}
+		}
+	
 		// TO-DO: check the size of time2, if it's 0, return false, otherwise return true
-//		return false;
-//	}
+//		return false; 
+		if(time2.size()==0){
+			return false;
+		}
+		else{
+			return true;
+		}
+	}
 	
 	public static int converts_to_minute(String day, String t){
 		return 1;
