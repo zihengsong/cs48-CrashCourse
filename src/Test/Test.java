d9b66685837b6027438f5602ac5cead82bc3fb1f
feiyang <feiyang@169-231-150-58.wireless.ucsb.edu>
2017-02-22 15:42:47 -0800

    more details for test

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -1,5 +1,17 @@
 package Test;
 
-public class Test {
+import ucsbCurriculum.*;
+import ucsbCurriculum.CourseScraper.CourseScraper;
+import ucsbCurriculum.Scheduler.Schedule;
 
+// This is environment for command line testing
+public class Test {
+	public CourseScraper scraper;
+	public Schedule scheduler;
+	
+	
+	// do the command line interaction 
+	public static void main(String[] args){
+		
+	}
 }
